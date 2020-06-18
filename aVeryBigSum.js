2dcc480372a9a94b45145f8eb69e4beee3201f80
ntr5 <pressntr@gmail.com>
2020-06-18 17:41:54 -0600

    created a function that takes an array of very large numbers and returns the sum

---
+++ aVeryBigSum.js
@@ -0,0 +1,9 @@
+function aVeryBigSum(ar) {
+    let sum = 0;
+    for (var i = 0; i < ar.length; i++) {
+     sum += ar[i]
+    }
+     return sum
+   }
+   
+   aVeryBigSum([1000,12342134,1234213,9879876])
